dedf90d72e1f39efdaa8b6494f4cff884da15076
zliu1Charlotte <zliu12@wpi.edu>
2025-09-08 10:37:38 -0400

    added ./src and editedinfo.ymal

---
+++ src/rtl/SRC/sub_module/arch_encoder.v
@@ -0,0 +1,7 @@
+//-------------------------------------------
+//	FPGA Synthesizable Verilog Netlist
+//	Description: Decoders for fabric configuration protocol
+//	Author: Xifan TANG
+//	Organization: University of Utah
+//	Date: Mon Jan 15 17:23:49 2024
+//-------------------------------------------
